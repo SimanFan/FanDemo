c5c391c0bfb4e2a51e285a8c5851a2e9dda6f7ad
范普森 <842025544@qq.com>
2022-02-13 15:21:27 +0800

    123

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿/****************************************************
+    文件：NewBehaviourScript.cs
+	作者：范普森
+    邮箱: 842025544@qq.com
+    日期：#CreateTime#
+	功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+
+}
